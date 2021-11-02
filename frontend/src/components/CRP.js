d3ab2a6f8d614650131fe8e43c7702ba5b266733
Daywick <derekli11204@gmail.com>
2021-11-02 17:54:26 -0400

    page rename

---
+++ frontend/src/components/CRP.js
@@ -0,0 +1,105 @@
+import React, { Component } from 'react';
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField"
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from "@material-ui/core/FormControl";
+import { Link } from "react-router-dom";
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+
+export default class CRP extends Component {
+    defaultInt = 2;
+
+    constructor(props) {
+        super(props);
+        this.state= {
+        CanVote: true,
+        VotesRequired: this.defaultInt,
+        };
+        this.handlebuttonpress = this.handlebuttonpress.bind(this);
+        this.handleCanVote = this.handleCanVote.bind(this);
+        this.handleVoteNum = this.handleVoteNum.bind(this);
+    }  
+
+    handleVoteNum(e) {
+        this.setState({
+            VotesRequired: e.target.value,
+        });
+    }
+
+    handleCanVote(e) {
+        this.setState({
+            CanVote: e.target.value === "true" ? true : false,
+
+        });
+    }
+    handlebuttonpress() {
+        const requestOptions = {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                votes_to_skip: this.state.VotesRequired,
+                can_pause: this.state.CanVote,
+            }),
+        };
+        fetch("/api/create-room", requestOptions)
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+    }
+
+    render() {
+        return <Grid container spacing={1}>
+            <Grid item xs={12} align="center">
+               <Typography component="h4" variant='h4'>
+                   Create Room
+               </Typography>
+               <FormControl component="fieldset">
+                   <FormHelperText>
+                       <div align="Center">
+                           Guest Control of Play back State.
+                       </div>
+                   </FormHelperText>
+                        <RadioGroup row defaultValue="true" onChange={this.handleCanVote}>
+                            <FormControlLabel
+                                value="true"
+                                control={ <Radio color="primary"/>}
+                                label="T/F"
+                                labelPlacement="bottom" />
+                            <FormControlLabel
+                                value="false"
+                                control={ <Radio color="secondary"/>}
+                                label="No Control"
+                                labelPlacement="bottom" />
+                        </RadioGroup>
+               </FormControl>
+            </Grid>
+                <Grid item xs={12} align="center">
+                    <FormControl>
+                        <TextField
+                            required={true} 
+                            type="number"
+                            onChange={this.handleVoteNum}
+                            defaultValue={this.defaultInt}
+                            inputProps={{min: 1,}}
+                        />
+                        <FormHelperText>
+                            <div align="center"> Int require for "skip" </div>
+                        </FormHelperText>
+                    </FormControl>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <Button color="secondary" variant="contained" onClick={this.handlebuttonpress}>
+                        Create the Room
+                    </Button>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <Button color="primary" variant="contained" to="/" component={Link}>
+                        Back
+                    </Button>
+                </Grid>
+                </Grid>
+    };
+}
---
+++ frontend/src/components/Room.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+export default class Example extends Component {
+    constructor(props) {
+        super(props);
+        this.state= {
+            
+        }
+    }
+}
